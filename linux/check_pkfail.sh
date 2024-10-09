7edf45c5257ebf6ca96bc21dc0464bb92899320c
honorasm <honorasm@outlook.com>
2024-10-09 23:08:44 +0800

    linux: check operating system

--- linux/check_pkfail.sh
+++ linux/check_pkfail.sh
@@ -1,1 +1,16 @@
-#/bin/bash
+#/bin/bash -e
+
+os_id() {
+  [ -e /etc/os-release ] && awk -F '=' '/^ID=/ {id=$2; sub(/^"/,"",id); sub(/"$/,"",id); print id}' /etc/os-release
+}
+
+case `os_id` in
+  debian)
+    echo "This is Debian."
+    ;;
+
+  *)
+    echo "Unsupported Operating System"
+    exit 1
+    ;;
+esac
