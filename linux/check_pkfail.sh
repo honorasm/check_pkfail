e7b52ed77ca9372ba8c10b31bd8659ccc4d1d7d5
honorasm <honorasm@outlook.com>
2024-10-10 19:14:58 +0800

    debian: install efitools package and check for pkfail test certificates

--- linux/check_pkfail.sh
+++ linux/check_pkfail.sh
@@ -1,12 +1,22 @@
 #/bin/bash -e
 
+# N.B. Now only the certificates in the PK, KEK and db UEFI variables are checked.
+
 os_id() {
   [ -e /etc/os-release ] && awk -F '=' '/^ID=/ {id=$2; sub(/^"/,"",id); sub(/"$/,"",id); print id}' /etc/os-release
 }
 
 case `os_id` in
   debian)
-    echo "This is Debian."
+    if ! command -v efi-readvar > /dev/null; then
+      sudo apt install -y efitools
+    fi
+    var_list="PK KEK db"
+    for var in ${var_list}; do
+        if efi-readvar -v $var | grep -qE 'DO NOT (TRUST|SHIP)'; then
+            echo "Test key FOUND."
+        fi
+    done
     ;;
 
   *)
